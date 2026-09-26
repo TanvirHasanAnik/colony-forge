27046fe8bd4afb260f8c67558657d36e323f0be5
Tanvir Hassan Anik <tanvir.h.anik@gmail.com>
2026-09-26 10:48:59 +0600

    Reintroduce TownhallIcon component as an SVG element with props support

--- src/components/common/icons/TownhallIcon.tsx
+++ src/components/common/icons/TownhallIcon.tsx
@@ -1,1 +1,17 @@
-src\components\common\icons\TownhallIcon.tsx
+import type { SVGProps } from "react";
+
+export function TownhallIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M3 21h18M5 21V7l7-4 7 4v14M9 21v-4a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v4M9 10h.01M15 10h.01" />
+    </svg>
+  );
+}
